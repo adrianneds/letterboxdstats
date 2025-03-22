64e4f0f936e212e1ec4be5805cd6a165ee7357d0
adrianneds <solisadrianned17@gmail.com>
2025-03-22 13:33:04 +0800

    chore: used list comprehension instead for creating diary df

--- main.py
+++ main.py
@@ -31,18 +31,15 @@ def main(username):
     data_df = init_dframe()
 
     for url in url_set:
-        data_df = diaryStats(data_df, url)
+        data_df = diarStats(data_df, url)
     
     print("==================================================================================================================")
     print( username + "'s Letterboxd Diary")
     print(data_df)
     print("==================================================================================================================")
 
-    print("Summary stats")
-    summary(data_df)
-    wf = watchFreq(data_df)
-    dfList = [data_df, wf]
-    return(dfList)
+    # print("Summary stats")
+    # summary(data_df)
 
 def init_dframe():
     # create a data frame
@@ -56,59 +53,55 @@ def init_dframe():
     }
     return(pd.DataFrame(diary))
 
-# diary stats of a single page
-def diaryStats(diary_df, url):
-    soup = init_soup(url)
+def getDate(entry):
+    # month and year
+    datetd = entry.find('td', class_='td-day diary-day center')
+    date = (datetd.find('a')['href'])[-11:-1]
+    return date
 
-    # diary entries
-    entries = soup.find_all('tr', class_="diary-entry-row") 
+def getFilmDetails(entry):
+    h3 = entry.find_all('h3', class_='headline-3 prettify')[0]
+    filmName = h3.find_all('a')[0].text
+    relDate = entry.find('td', class_="td-released center").find('span').text
+    return [filmName, relDate]
 
-    # iterate through entries
-    for i in range(0,len(entries)):
+def getRating(entry):
+    rating = entry.find_all('span', class_="rating")
+    rated = rating[0].get("class")
+    if len(rated)>1:
+        userRating = int(float(rated[1][6:]))/2
+    else:
+        userRating = pd.NA
+    return userRating
 
-        # month and year
-        date_div = entries[i].find('div', class_='date')
-        if date_div is not None:
-            month_year = date_div.find('strong').text + " " + date_div.find('small').text
-
-        # film name
-        h3 = entries[i].find_all('h3', class_='headline-3 prettify')[0]
-        filmName = h3.find_all('a')[0].text
-        # # print(filmName)
-
-        # film rating
-        rating = entries[i].find_all('span', class_="rating")
-        rated = rating[0].get("class")
-        if len(rated)>1:
-            userRating = int(float(rated[1][6:]))/2
-        else:
-            userRating = pd.NA
-
-        # release date
-        relDate = entries[i].find('td', class_="td-released center").find('span').text
-
-        # liked ?
+def getLikeReview(entry):
+    liked = False
+    liked_data = entry.find('td', class_='td-like center diary-like').find('span', class_="icon-liked")
+    if liked_data is not None:
+        liked = True
+    else:
         liked = False
-        liked_data = entries[i].find('td', class_='td-like center diary-like').find('span', class_="icon-liked")
-        if liked_data is not None:
-            liked = True
-        else:
-            liked = False
 
-        # reviewed ?
+    review = False
+    review_data = entry.find('td', class_='td-review').find('a')
+    if review_data is not None:
+        review = True
+    else:
         review = False
-        review_data = entries[i].find('td', class_='td-review').find('a')
-        if review_data is not None:
-            review = True
-        else:
-            review = False
 
-        # add info to data frame
-        newRow = {'film_name': filmName, 'film_rating':userRating, 
-                "release_date": relDate, "liked":liked, "reviewed":review, "month_year": month_year}
-        diary_df = pd.concat([diary_df, pd.DataFrame([newRow])], ignore_index=True)
+    return [liked, review]
 
-    return(diary_df)
+def diarStats(diary_df, url):
+    soup = init_soup(url)
+    entries = soup.find_all('tr', class_="diary-entry-row") 
+    newRows = [pd.DataFrame([ {'film_name': getFilmDetails(li)[0], 'film_rating': getRating(li),
+                            "release_date": getFilmDetails(li)[1], "liked":getLikeReview(li)[0],
+                            "reviewed":getLikeReview(li)[1], "month_year": getDate(li)} ]) for li in entries]
+    diary_1 = pd.concat( newRows , ignore_index=True)
+    diary_df = pd.concat([diary_df, diary_1], ignore_index=True)
+
+      #      diary_df = pd.concat([diary_df, pd.DataFrame([newRow])], ignore_index=True)
+    return diary_df
 
 # summary stats
 def summary(df):
@@ -136,20 +129,5 @@ def summary(df):
     review_df = pd.DataFrame(df.groupby(['reviewed']).size())
     review_df.columns=["count"]
     print("..and reviewed " + str( round( (review_df.at[1,"count"]/ review_df["count"].sum())*100 ,2 ) ) + "% of your watched films")
-
-def watchFreq(df):
-    line_df = pd.DataFrame(df.groupby(['month_year']).count()['film_name'])
-    heat_df = line_df
-    fig = go.Figure(data=[go.Scatter(  x=line_df.index, y=line_df['film_name'] )])
-
-    heat_df = heat_df.reset_index()
-    heat_df[['month', 'year']] = heat_df['month_year'].str.split(' ',expand=True)
-    heat_df = heat_df.sort_values(by=['year'], ascending=True)
-    print(heat_df)
-
-    # fig2 = px.imshow([  ])
-
-    # figs = [fig, fig2]
-    return(fig)
 
 main("riannecantos")
